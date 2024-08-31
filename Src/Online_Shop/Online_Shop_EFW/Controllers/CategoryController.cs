a49a92e9bb8eb094f8efac1d9e2479d961d37953
Nimur <nimur40@gmail.com>
2024-08-31 07:44:47 +0600

    add 15 video

--- Src/Online_Shop/Online_Shop_EFW/Controllers/CategoryController.cs
+++ Src/Online_Shop/Online_Shop_EFW/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Online_Shop_EFW.Data;
+using Online_Shop_EFW.Models;
 
 namespace Online_Shop_EFW.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList=_db.Categories.ToList(); 
+            return View(objCategoryList);
         }
     }
 }
